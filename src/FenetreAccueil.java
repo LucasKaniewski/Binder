46320e03a54276241bba4960d7b36e10234f252a
Alphouille <90383513+LucasKaniewski@users.noreply.github.com>
2022-01-13 11:51:13 +0100

    indent

--- src/FenetreAccueil.java
+++ src/FenetreAccueil.java
@@ -20,46 +20,46 @@ public class FenetreAccueil extends JFrame implements ActionListener {
 	
 	public FenetreAccueil() {
 			
-			this.setTitle("BINDER");
-			this.setLocationRelativeTo(null);
-	    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    	this.setSize(800, 600);
-	    	this.setResizable(false);
+		this.setTitle("BINDER");
+		this.setLocationRelativeTo(null);
+	   	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	   	this.setSize(800, 600);
+	    this.setResizable(false);
+    	
+	    // Instanciation de la barre de menu
+	    JMenuBar menu= new JMenuBar();
 	    	
-	    	// Instanciation de la barre de menu
-	    	JMenuBar menu= new JMenuBar();
-	    	
-	    	// Instanciation des "boutons" dans la barre de menu
-	    	JMenu menuCreation = new JMenu("Facture");
+	    // Instanciation des "boutons" dans la barre de menu
+	    JMenu menuCreation = new JMenu("Facture");
 	        
-	        // Instanciation d'élément du menu Facture
-	        this.creeFacture = new JMenuItem("Créer une facture");
-	    	this.ajouterTache = new JMenuItem("Ajouter une tâche");
-	        this.supprElement = new JMenuItem("Supprimer un élément");
-	        this.modifierFacture = new JMenuItem("Modifier une facture");
+	    // Instanciation d'élément du menu Facture
+	    this.creeFacture = new JMenuItem("Créer une facture");
+	    this.ajouterTache = new JMenuItem("Ajouter une tâche");
+	    this.supprElement = new JMenuItem("Supprimer un élément");
+	    this.modifierFacture = new JMenuItem("Modifier une facture");
 	               
-	        // Ajout d'élément au menu Facture
-	        menuCreation.add(creeFacture);
-	        menuCreation.add(ajouterTache);
-	        menuCreation.add(supprElement);
-	        menuCreation.add(modifierFacture);
+	    // Ajout d'élément au menu Facture
+	    menuCreation.add(creeFacture);
+	    menuCreation.add(ajouterTache);
+	    menuCreation.add(supprElement);
+	    menuCreation.add(modifierFacture);
 	        
 	        
-	        // Ajout des "bouton" menu à la barre de menu
-	        menu.add(menuCreation);
+	    // Ajout des "bouton" menu à la barre de menu
+	    menu.add(menuCreation);
 
 	        
 	        
-	        // Ecoute des JButton menu circuit 
-	        creeFacture.addActionListener(this);
-	        ajouterTache.addActionListener(this);
-	        supprElement.addActionListener(this);
-	        modifierFacture.addActionListener(this);
+	    // Ecoute des JButton menu circuit 
+	    creeFacture.addActionListener(this);
+	    ajouterTache.addActionListener(this);
+	    supprElement.addActionListener(this);
+	    modifierFacture.addActionListener(this);
 	        
-	        // Permet de définir le menu utilisé dans la JFrame
-	        this.setJMenuBar(menu);	        
-	        this.setAlwaysOnTop(true);
-	        this.setVisible(true);
+	    // Permet de définir le menu utilisé dans la JFrame
+	    this.setJMenuBar(menu);	        
+	    this.setAlwaysOnTop(true);
+	    this.setVisible(true);
 
 	}
 	
