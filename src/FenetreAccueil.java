a8a89fef3bc442520581c1f4d6ba8b26735e27e8
Alphouille <90383513+LucasKaniewski@users.noreply.github.com>
2022-01-13 11:49:16 +0100

    Ajout premier menu

--- src/FenetreAccueil.java
+++ src/FenetreAccueil.java
@@ -11,10 +11,10 @@ import javax.swing.*;
 public class FenetreAccueil extends JFrame implements ActionListener {
 	
 	// JMenuItem 
-	private JMenuItem addFacture;
-	private JMenuItem modifFacture;
-	private JMenuItem affichageFacture;
-	private JMenuItem searchFacture;
+	private JMenuItem creeFacture;
+	private JMenuItem ajouterTache;
+	private JMenuItem supprElement;
+	private JMenuItem modifierFacture;
 	
 
 	
@@ -30,36 +30,31 @@ public class FenetreAccueil extends JFrame implements ActionListener {
 	    	JMenuBar menu= new JMenuBar();
 	    	
 	    	// Instanciation des "boutons" dans la barre de menu
-	    	JMenu menuFacture = new JMenu("Facture");
-
-
+	    	JMenu menuCreation = new JMenu("Facture");
 	        
-	        // Instanciation d'élément du menu Circuit
-	        this.affichageFacture = new JMenuItem("Recherche une facture");
-	    	this.addFacture = new JMenuItem("Ajouter une facture");
-	        this.modifFacture = new JMenuItem("Modifier une facture");
-	        this.searchFacture = new JMenuItem("Rechercher une facture");
-
-	       
-	
-	        
-	        // Ajout d'élément au menu Circuit
-	        menuFacture.add(affichageFacture);
-	        menuFacture.add(addFacture);
-	        menuFacture.add(modifFacture);
-	        menuFacture.add(searchFacture);
+	        // Instanciation d'élément du menu Facture
+	        this.creeFacture = new JMenuItem("Créer une facture");
+	    	this.ajouterTache = new JMenuItem("Ajouter une tâche");
+	        this.supprElement = new JMenuItem("Supprimer un élément");
+	        this.modifierFacture = new JMenuItem("Modifier une facture");
+	               
+	        // Ajout d'élément au menu Facture
+	        menuCreation.add(creeFacture);
+	        menuCreation.add(ajouterTache);
+	        menuCreation.add(supprElement);
+	        menuCreation.add(modifierFacture);
 	        
 	        
 	        // Ajout des "bouton" menu à la barre de menu
-	        menu.add(menuFacture);
+	        menu.add(menuCreation);
 
 	        
 	        
 	        // Ecoute des JButton menu circuit 
-	        addFacture.addActionListener(this);
-	        modifFacture.addActionListener(this);
-	        affichageFacture.addActionListener(this);
-	        searchFacture.addActionListener(this);
+	        creeFacture.addActionListener(this);
+	        ajouterTache.addActionListener(this);
+	        supprElement.addActionListener(this);
+	        modifierFacture.addActionListener(this);
 	        
 	        // Permet de définir le menu utilisé dans la JFrame
 	        this.setJMenuBar(menu);	        
@@ -77,7 +72,7 @@ public class FenetreAccueil extends JFrame implements ActionListener {
 			 * Repaint des panels Facture
 			
 		
-			if(e.getSource().equals(afficherSpectateur)) { 
+			if(e.getSource().equals(creeFacture)) { 
 				this.getContentPane().removeAll();
 				this.getContentPane().add(new PanelAfficherSpectateur(Modele.affichageSpectateur()).getMonPanel());
 				this.getContentPane().revalidate();
