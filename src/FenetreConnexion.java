f43870383d8aea13363342e23b07108f711ed5b2
Azzraelll <camposmickaelpro@gmail.com>
2022-01-21 12:18:22 +0100

    1er

--- src/FenetreConnexion.java
+++ src/FenetreConnexion.java
@@ -1,104 +1,160 @@
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.*;
-
 /**
  * 
  * @author lucas_kaniewski | mickael_campos
  *
  */
 
-public class FenetreConnexion extends JFrame implements ActionListener {
-	
-	//JPanel
-    private JPanel monPanel;
-    private JPanel monPanelGlobal;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-	// JLabel
-	private JLabel lblIdentifiant;
-	private JLabel lblMotDePasse;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class FenetreConnexion extends JFrame implements ActionListener{
+
+	//les panels
+ 	private JPanel monPanelGlobal;
+    private JPanel monPanel;
+	private JPanel monPanelBas;
 	
-	// JTextField
-	private JTextField jtextIdentifiant;
-	private JPasswordField jpassMdp;
+	private JLabel lblTitre;
+	private JLabel lblID;
+	private JLabel lblMDP;
+	private JLabel lblMsgErreur;
+	
+	private JTextField jtfID;
+	private JPasswordField jtfMDP;
     private char getpass[];
-    
-	// Jbutton
-	private JButton btnConnexion;
 	
+	private JButton btnConnexion;
+	private JButton btnQuit;
+	
+	
+	//CONSTRUCTEUR
 	public FenetreConnexion() {
 		
-		this.setTitle("BINDER");
-    	this.setLocationRelativeTo(null);
-    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	this.setSize(600, 400);
-    	this.setResizable(false);
-	     
-		// Instanciation des panels
-        this.monPanel = new JPanel();
-        this.monPanelGlobal = new JPanel();
+    	//configuration de la fenetre
+    	this.setTitle("SARL BINDER - Connexion");
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500, 400);
+        this.setLocation(600, 300);
+		this.setResizable(false);
+		
+		//================================ Les panel ===============================
+		
+		this.monPanel = new JPanel();
+		this.monPanel.setBackground(new Color(86, 115, 154));
+		this.monPanel.setLayout(new BorderLayout());
+		this.monPanel.setLayout(new GridLayout(12, 1));
+		this.monPanel.setForeground(Color.red);
+		
+		this.monPanelBas = new JPanel();
+		this.monPanelBas.setLayout(new BorderLayout());
+		this.monPanelBas.setLayout(new GridLayout(1, 2));
+		this.monPanelBas.setBackground(new Color(86, 115, 154));
+		
+		this.monPanelGlobal = new JPanel();
+		this.monPanelGlobal.setBackground(new Color(86, 115, 154));
+		
+		
+		//================================ L'affichage =============================
+		
+		//TITRE 
+		
+		this.lblTitre = new JLabel ();
+		this.lblTitre.setForeground(Color.white);
+		this.lblTitre.setText("CONNEXION");
+		this.monPanel.add(this.lblTitre, BorderLayout.CENTER);
+    	
+    	Font f = new Font("Sherif", Font.BOLD, 18);    
+    	this.lblTitre.setFont(f);
+        this.monPanel.add(this.lblTitre);
         
-        // Set Layout des panels
-        this.monPanel.setLayout(new FlowLayout());
-        this.monPanelGlobal.setLayout(new FlowLayout());  
-                
-        // Instanciation des JLabel
-        this.lblIdentifiant = new JLabel("Identifiant :") ;
-        this.lblMotDePasse = new JLabel("Mot de Passe :") ;
         
-        // Instanciation des JTextField
-        this.jtextIdentifiant = new JTextField("");
-        this.jpassMdp = new JPasswordField("");
-        
-        // Création des box pour entrée les info
-        this.jtextIdentifiant.setPreferredSize(new Dimension(150,20));
-        this.jpassMdp.setPreferredSize(new Dimension(150, 20));
-
-        // JButton
-        this.btnConnexion = new JButton ("Connexion");
-        this.btnConnexion.addActionListener(this);
-
-        //Ajout des éléments au panel central
-        this.monPanel.add(lblIdentifiant);
-        this.monPanel.add(jtextIdentifiant);
-        this.monPanel.add(lblMotDePasse);
-        this.monPanel.add(jpassMdp);
-        this.monPanel.add(btnConnexion);
-        
-        //Ajout de l'élément titre au panel titre 
-
-        this.setAlwaysOnTop(true);
-        this.getContentPane().add(monPanel);
-        this.setVisible(true);
-        
-        // Layout des panels
-        this.monPanelGlobal.add(monPanel, BorderLayout.NORTH);
-        this.getContentPane().add(this.monPanelGlobal);
-    }
-
-	@Override
-
+		//INSERTION ID
+		
+        this.lblID= new JLabel ();
+        this.lblID.setForeground(Color.white);
+        this.lblID.setText("Identifiant :");
+        this.monPanel.add(this.lblID, BorderLayout.CENTER);
+  
+        this.jtfID = new JTextField(""); 
+        this.jtfID.setPreferredSize(new Dimension(250, 30));
+        this.monPanel.add(this.jtfID);
+    	
+		//INSERTION MDP
+		
+        this.lblMDP = new JLabel ();
+        this.lblMDP.setForeground(Color.white);
+        this.lblMDP.setText("Mot de Passe :");
+        this.monPanel.add(this.lblMDP, BorderLayout.CENTER);
+    	
+        this.jtfMDP = new JPasswordField(""); 
+        this.jtfMDP.setPreferredSize(new Dimension(250, 30));
+        this.jtfMDP.setForeground(Color.black);
+        this.monPanel.add(this.jtfMDP);
+  		
+  		//BUTTON
+		
+        this.btnConnexion = new JButton("Connection");
+        this.monPanelBas.add(this.btnConnexion);
+    	
+        this.btnQuit = new JButton("Quiter");
+        this.monPanelBas.add(this.btnQuit);
+    	
+		
+    	//Les ecoute des button
+    	this.btnConnexion.addActionListener(this);
+    	this.btnQuit.addActionListener(this);
+    	
+    	
+    	this.lblMsgErreur= new JLabel ();
+    	this.lblMsgErreur.setText("");
+    	this.lblMsgErreur.setForeground(Color.RED);
+    	this.monPanel.add(this.lblMsgErreur, BorderLayout.CENTER);
+    	
+    	
+    	
+    	this.monPanelGlobal.add(this.monPanel);
+    	this.monPanel.add(this.monPanelBas);
+    	
+    	this.getContentPane().add(this.monPanelGlobal);
+    	this.setVisible(true);
+	}
 	
-	public void actionPerformed (ActionEvent evenement) {
-    	if(evenement.getSource() == btnConnexion) { //Si JButton connexion est cliqué
-    		String login = jtextIdentifiant.getText();
-    		getpass = jpassMdp.getPassword();
+	
+	public void actionPerformed(ActionEvent e) {
+		if ( e.getSource().equals(this.btnConnexion)) {
+			
+			String id = jtfID.getText();
+			this.getpass = jtfMDP.getPassword();
     		String mdp = String.valueOf(getpass);
-    		if(Modele.connecter(login, mdp)) {
-    			this.dispose();
-    			FenetreAccueil uneAutreFenetre = new FenetreAccueil(); // Instanciationd e la fenetre d'accueil
-    		}
-    		else {
-    			JLabel reponse = new JLabel("Erreur");	
-    			this.monPanel.add(reponse);
-    			monPanel.revalidate();
-    			monPanel.repaint();
-    		}
-    	}
-    }
+			
+			if(Modele.connecter(id , mdp)) {
+        		this.dispose();
+        		FenetreAccueil accueil = new FenetreAccueil();
+			}
+			else {
+				this.lblMsgErreur.setText("identifiant incorecte");
+			}
+			
+		}
+		else {
+			this.dispose();
+		}
+		
+	}
 	
 	
 
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,15 @@
  *
  */
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedLookAndFeelException{
+
+		UIManager.setLookAndFeel(new NimbusLookAndFeel());
 		
 		FenetreConnexion uneFenetre = new FenetreConnexion();
 		
